d46a2f57c9aeb1490db992a85f012fabc7f6676a
Arpan Bhattacharya <bhattacharyada@gmail.com>
2020-11-19 23:30:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let omShanti =  document.getElementById('omShanti');
 let popper = document.getElementById('popper');
-let popBtn = document.getElementById('popBtn');
+let arpanFirst = document.getElementById('arpanFirst');
 
 function playPop()
 {
 	popper.play();
+	arpanFirst.style.display = 'none';
 }
 
 
