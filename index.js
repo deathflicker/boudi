6b28daf4f07614084c39afa9cab99d70f819fc12
Arpan Bhattacharya <bhattacharyada@gmail.com>
2020-11-19 23:22:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let omShanti =  document.getElementById('omShanti');
+let popper = document.getElementById('popper');
+let popBtn = document.getElementById('popBtn');
 
-
-
+function playPop()
+{
+	popper.play();
+}
 
 
 function playSong(){
 	omShanti.play();
 }
+
+popBtn.click();
